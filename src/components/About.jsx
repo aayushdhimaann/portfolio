c8ee3df862f35b2b8c436458d3083eef3af1f34f
Aayush Dhiman <127728546+aayushdhimaann@users.noreply.github.com>
2024-09-04 17:39:01 +0530

    fixed about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -28,8 +28,7 @@ const About = () => {
           </div> */}
           <div className="about-me">
             <p>
-              I'm a <b>Frontend Web Developer</b> building the Front-end of
-              Websites and Web Applications that leads to the success of the
+              I'm a <b>Web Developer</b> building the Websites and Web Applications that leads to the success of the
               overall product. Check out some of my work in the <b>Projects</b>{" "}
               section. <br />
               <br /> I also like sharing content related to the stuff that I
